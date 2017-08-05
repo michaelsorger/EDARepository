79c8bbfe2e20e8e24f30d15f8acf61cd2454f458
Lee Neuschwander <lneuschwander-777@hotmail.com>
2017-08-04 23:49:48 -0600

    Final Push. Congrats and nice job everyone!!

---
+++ EDARepoProject/Assets/Scripts/ParticleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeOf(ParticleSystem))]
+
+public class ParticleStarter : MonoBehaviour
+{
+
+    private ParticleSystem _psystem;
+
+    void Awake()
+    {
+        _psystem = GetComponent<ParticleSystem>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        _psystem.Play();
+    }
+
+
+
+// Update is called once per frame
+void Update () {
+		
+	}
+}
